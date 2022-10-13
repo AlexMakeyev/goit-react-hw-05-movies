815c3be7edfca6088159aab4a828e549874f9537
Alex Makeyev <makeyev1987@gmail.com>
2022-10-13 11:39:03 +0300

    Update Movies.styled.jsx

--- src/pages/Movies/Movies.styled.jsx
+++ src/pages/Movies/Movies.styled.jsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
-const setHeight = ({ value }) => {
-  if (value !== '') {
-    return 'width:100%';
+const setHeight = ({ query }) => {
+  if (query !== '') {
+    return '100%';
+  } else {
+    return '100vh';
   }
 };
 export const MainContainer = styled.main`
