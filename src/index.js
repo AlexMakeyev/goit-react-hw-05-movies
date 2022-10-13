52a6f32b0210dbdaac97e6ae55f2508a7005240d
Alex Makeyev <makeyev1987@gmail.com>
2022-10-13 13:23:57 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter } from 'react-router-dom'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/goit-react-hw-05-movies/">
       <App />
-    </BrowserRouter>
+    </BrowserRouter>  
   </React.StrictMode>
 );
